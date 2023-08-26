5fd719f89551366132d971f596477f4231334e31
Thomas <0520wns@naver.com>
2023-08-27 02:32:38 +0900

    평면에 대해 카메라 회전?

--- main.py
+++ main.py
@@ -11,51 +11,66 @@ pygame.display.set_caption("Display")
 
 fps = 240
 clock = pygame.time.Clock()
-pygame.key.set_repeat(1)
+pygame.key.set_repeat(10)
 
 
 mapCord = []
-n = 100
-for i in range(n + 1):
-    for j in range(n + 1):
-        mapCord.append([i - n / 2, j - n / 2, 0])
+n = 200
+for j in range(n + 1):
+    for i in range(n + 1):
+        mapCord.append([i - n / 2, j - n / 2])
 
 
 def dist(pos1, pos2):
-    return math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2 + (pos1[2] - pos2[2]) ** 2)
+    return math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)
 
 
 class Camera:
     def __init__(self):
-        self.pos = [0, 0, 60]
-        self.angleX = math.pi / 180 * 30  # x 시야각
-        self.angleY = math.atan(math.tan(self.angleX / 2) * screenSize[1] / screenSize[0]) * 2  # y 시야각
-        self.focalLength = 300
+        self.pos = [0, 0, -100]
+        self.angle = [0, 0]
+        self.focalLength = -100
+        self.pov = math.pi / 180 * 120
 
     def display(self):
         for i in mapCord:
-            dx = i[0] - self.pos[0]
-            dy = i[1] - self.pos[1]
-            dz = i[2] - self.pos[2]
+            d = [0, 0]
+            cor = [0, 0]
+            tan = [0, 0]
+            condition = [0, 0]
 
-            x = self.focalLength * dx / dz
-            y = self.focalLength * dy / dz
+            for j in range(2):
+                d[j] = i[j] - self.pos[j]
+                if self.angle[j] > math.pi:
+                    self.angle[j] = math.pi * -1
+                elif self.angle[j] < math.pi * -1:
+                    self.angle[j] = math.pi
+                tan[j] = math.atan(d[j] / self.pos[2]) + self.angle[j]
+                if self.pov / 2 >= tan[j] >= self.pov / -2:
+                    condition[j] = 1
+                    cor[j] = self.focalLength * math.tan(tan[j])
 
-            y *= screenSize[0] / screenSize[1]
-
-            pygame.draw.circle(screen, (0, 0, 0), [x + screenSize[0] / 2, y + screenSize[1] / 2], 1)
+            if 0 in condition:
+                continue
+            pygame.draw.circle(screen, (0, 0, 0), [cor[0] + screenSize[0] / 2, cor[1] * -1 + screenSize[1] / 2], 1)
 
     def move(self):
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                self.angle[0] += math.pi / 180 * 1
+            if event.key == pygame.K_a:
+                self.angle[0] -= math.pi / 180 * 1
             if event.key == pygame.K_w:
-                self.pos[2] += 0.1
-                print(self.pos[2])
+                self.angle[1] += math.pi / 180 * 1
             if event.key == pygame.K_s:
-                self.pos[2] -= 0.1
-                print(self.pos[2])
+                self.angle[1] -= math.pi / 180 * 1
+            if event.key == pygame.K_0:
+                self.pos[2] += 1
+            if event.key == pygame.K_9:
+                self.pos[2] -= 1
+
 
 James = Camera()
-
 
 while True:
     clock.tick(fps)
